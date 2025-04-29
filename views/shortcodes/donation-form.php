67d43f28419eb0dfe123097807b720ca0a51d816
Sujit Sarkar <58522421+iamsujitsarkar@users.noreply.github.com>
2025-04-29 10:41:16 +0600

    fixed subscription donation

--- views/shortcodes/donation-form.php
+++ views/shortcodes/donation-form.php
@@ -7,7 +7,7 @@
 ?>
 
 <div class="edp-alert-element edp-d-none">
-    <div class="edp-alert edp-alert-primary text-center rounded-0" role="alert" id="edp-alert-message">
+    <div class="edp-alert edp-alert-primary edp-text-center rounded-0" role="alert" id="edp-alert-message">
         <i>&quest;</i>
         <span><?php esc_html_e('Item added to cart successfully', 'ehx-donate'); ?></span>
     </div>
